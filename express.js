45c7a2bc4b032339e549fe06d214d3a51bb2b051
Mateusz Rusin <mateusz.rusin@pearson.com>
2017-04-21 18:00:59 +0200

    Express improve

--- express.js
+++ express.js
@@ -1,86 +1,70 @@
 'use strict';
 
-const HOST = 'localhost';
 const PORT = 3000;
 const PHOTOS = 'img';
 
 var express = require('express');
 var app = express();
+
+var server = app.listen(PORT, function() {
+    console.log('Listening on port ' + PORT)
+});
+
+/** PEER **/
 var ExpressPeerServer = require('peer').ExpressPeerServer;
-var Mongojs = require('mongojs');
-var Fs = require('fs');
-var Path = require('path');
-var Db = Mongojs('mongodb://localhost:27017/snooker', ['games']);
-var multer  = require('multer');
+app.use('/peer', ExpressPeerServer(server, {debug: true}));
+
+/** GAME **/
 var bodyParser = require('body-parser');
+var Mongojs = require('mongojs');
+var Db = Mongojs('mongodb://localhost:27017/snooker', ['games']);
 var joi = require('joi');
-//  Initialize valid-express module
 var validExpress = require('valid-express');
 var validator = validExpress();
-
-
-var server = app.listen(PORT, function () {
-    console.log('Listening on port ' + PORT)
-})
-
-var options = {
-    debug: true
-}
-
-app.use('/peer', ExpressPeerServer(server, options));
-
-app.use(bodyParser.json());
-
 var bodySchema = {
     body: {
-        player1: joi.object().required(),
-        player2: joi.object().required(),
+        player1: joi.object().keys({
+            name: joi.string(),
+            photo: joi.string().allow(null)
+        }),
+        player2: joi.object().keys({
+            name: joi.string(),
+            photo: joi.string().allow(null)
+        }),
         referee: joi.object().required(),
         frames: joi.number().integer().min(1).max(100),
         title: joi.string().allow('')
     }
 };
-
-
-// save data to mongo
+app.use(bodyParser.json());
 app.post('/game', validator.validate(bodySchema), function(request, response) {
-
-    const data = (request.body);
-
-    Db.games.save(data, function (err, doc) {
+    Db.games.save(request.body, function (err, doc) {
         response.send(doc._id.toString());
     });
 });
-
-// get data from mongo
 app.get('/game/:id', function (request, response) {
-
-    const data = { _id: Mongojs.ObjectId(request.params.id) };
-
-    Db.games.findOne(data, function(err, doc) {
+    Db.games.findOne({_id: Mongojs.ObjectId(request.params.id)}, function(err, doc) {
         response.send(doc)
     });
 });
 
-var upload = multer({dest: 'img/'});
-
-//save uploaded image
+/** UPLOAD **/
+var Fs = require('fs');
+var Path = require('path');
+var multer  = require('multer');
+var upload = multer({dest: PHOTOS});
 app.post('/upload', upload.single('file'), function(request, response) {
-
-    const name = Date.now() + Path.parse(request.file.originalname).ext;
-    const path = [__dirname, PHOTOS, name].join('/');
-
+    var name = Date.now() + Path.parse(request.file.originalname).ext;
+    var path = [__dirname, PHOTOS, name].join('/');
     Fs.rename(request.file.path, path, function(err) {
         if (err) {
             console.log(err);
         }
-
         response.send(name);
     });
-
 });
 
+/** STATIC **/
 app.use('/img', express.static('img'));
 app.use('/asset', express.static('asset'));
-
 app.use('/', express.static('dist'))
